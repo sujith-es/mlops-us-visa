ee8b67d12cfedecc0c9ffd08fc04f77797b0393a
sujith.esurendran@ust.com <sujith.esurendran@ust.com>
2024-12-03 20:33:17 +0530

    mongo db client

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.1.0",
+    author="Sujith",
+    author_email="sujith.learnearn@gmail.com",
+    packages=find_packages()
+)
---
+++ test.py
@@ -0,0 +1,28 @@
+import pymongo
+import pandas as pd
+
+DB_NAME = "US_VISA"
+COLLECTION_NAME = "visa_data"
+CONNECTION_URL = "mongodb+srv://sujithlearnearn:lkevaBQjv2sGmdy8@clusterus-visa.ca8l7.mongodb.net/?retryWrites=true&w=majority&appName=Clusterus-visa"
+
+from pymongo.mongo_client import MongoClient
+uri = "mongodb+srv://sujithlearnearn:lkevaBQjv2sGmdy8@clusterus-visa.ca8l7.mongodb.net/?retryWrites=true&w=majority&appName=Clusterus-visa"
+# Create a new client and connect to the server
+client = MongoClient(uri)
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# df = pd.read_csv("notebook/Visadataset.csv")
+# data = df.to_dict(orient="records")
+
+# client = pymongo.MongoClient(CONNECTION_URL, tls=False)
+# data_base = client[DB_NAME]
+# collection = data_base[COLLECTION_NAME]
+
+# rec = collection.insert_many(data)
+# print(rec)
